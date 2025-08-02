fd97a83350ccc3adfe6e007132584541eeef46f1
Ishan Misra <ishanm1603@gmail.com>
2025-08-02 19:37:39 +0530

    Fix routing for Auth

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -27,7 +27,19 @@ export function AuthProvider({children}: {children : ReactNode}) {
             setUser(session?.user ?? null);
             setLoading(false);
         })
+
+        const {data: {subscription}} = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+            setUser(session?.user ?? null);
+            setLoading(false);
+        })
+
+        return () => {
+            subscription.unsubscribe();
+        }
+
     }, [])
+
 
     async function signOut() {
         await supabase.auth.signOut();
--- lib/middleware.ts
+++ lib/middleware.ts
@@ -49,6 +49,12 @@ if(request.nextUrl.pathname === "/signin" && user) {
   return NextResponse.redirect(url)
 }
 
+if(request.nextUrl.pathname === "/dashboard" && !user) {
+  const url = request.nextUrl.clone()
+  url.pathname = '/signin'
+  return NextResponse.redirect(url)
+}
+
 //   if (
 //     !user &&
 //     !request.nextUrl.pathname.startsWith('/login') &&
