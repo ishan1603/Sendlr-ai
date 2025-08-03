47c70ac0594b20e0e02233381431e7ef4f8155c0
Ishan Misra <ishanm1603@gmail.com>
2025-08-03 16:57:15 +0530

    Implement News API

--- lib/inngest/functions/scheduled-newsletter.ts
+++ lib/inngest/functions/scheduled-newsletter.ts
@@ -1,7 +1,14 @@
 import { inngest } from "../client";
+import { fetchArticles } from "@/lib/news";
 
 export default inngest.createFunction(
   { id: "newsletter/scheduled" },
   { event: "newsletter.schedule" },
-  async () => {}
+  async ({ event, step, runId }) => {
+    const allArticles = await step.run("fetch-news", async () => {
+      const categories = ["technology", "business", "politics"];
+
+      return fetchArticles(categories);
+    });
+  }
 );
